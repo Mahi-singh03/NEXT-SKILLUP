9857a91b6b635709ab57cc1234eb54cc6f0698a9
Mahi singh <aka.mahisingh@gmail.com>
2025-07-10 15:23:48 +0530

    gallert works

--- src/app/(Main App)/gallery/page.js
+++ src/app/(Main App)/gallery/page.js
@@ -24,7 +24,7 @@ const Gallery = () => {
       setError(null);
 
       const response = await fetch('/api/cloudinary', {
-        method: 'POST',
+        method: 'GET',
         headers: {
           'Content-Type': 'application/json',
         },
